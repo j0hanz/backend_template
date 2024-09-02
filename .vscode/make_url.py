0d1f653bf6cff326fb12f38217fc1f309a46bbea
Linus Johansson <159924955+j0hanz@users.noreply.github.com>
2024-09-02 23:29:07 +0200

    Update make_url.py

--- .vscode/make_url.py
+++ .vscode/make_url.py
@@ -23,27 +23,26 @@ def extract_cloudinary_credentials(file_path):
         cloud_name = re.findall(r"'(.*?)'", content[15])[0]
         api_key = re.findall(r"'(.*?)'", content[16])[0]
         api_secret = re.findall(r"'(.*?)'", content[17])[0]
-
         return cloud_name, api_key, api_secret
 
     except FileNotFoundError:
-        print(f"Error: The file {file_path} does not exist.")
+        print(f'Error: The file {file_path} does not exist.')
         sys.exit(1)
     except IndexError:
         print(
-            f"Error: The file {file_path} does not contain the expected format."
+            f'Error: The file {file_path} does not contain the expected format.'
         )
         sys.exit(1)
     except Exception as e:
-        print(f"An unexpected error occurred: {e}")
+        print(f'An unexpected error occurred: {e}')
         sys.exit(1)
 
 
 def main():
-    file_path = "cloudinary_python.txt"
+    file_path = 'cloudinary_python.txt'
     cloud_name, api_key, api_secret = extract_cloudinary_credentials(file_path)
-    print(f"cloudinary://{api_key}:{api_secret}@{cloud_name}")
+    print(f'cloudinary://{api_key}:{api_secret}@{cloud_name}')
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
